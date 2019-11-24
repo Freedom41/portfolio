38514fc9c166f750428d28343947e37a69e828fa
Mark <s.markmas@gmail.com>
2019-11-25 03:11:11 +0530

    Updates

---
+++ precache-manifest.21ec22f4307cc7ed5a86f7677003fe7b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b0240bac0968e520ae3e9c9feff26e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8618e752da4d80741f03",
+    "url": "/portfolio/static/css/main.4fd0c15f.chunk.css"
+  },
+  {
+    "revision": "81e5188ea8c9a3daeeaa",
+    "url": "/portfolio/static/js/2.3b8bb79b.chunk.js"
+  },
+  {
+    "revision": "8618e752da4d80741f03",
+    "url": "/portfolio/static/js/main.f256ab1b.chunk.js"
+  },
+  {
+    "revision": "6b26cb3bc1b9147ba086",
+    "url": "/portfolio/static/js/runtime-main.86a05020.js"
+  }
+]);
